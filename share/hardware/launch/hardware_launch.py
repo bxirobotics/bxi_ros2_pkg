6bb1317dbdb23dce9eb188bb0697f8dfa320f5cb
liufq <popsay@163.com>
2025-03-28 16:58:15 +0800

    first commit

---
+++ share/hardware/launch/hardware_launch.py
@@ -0,0 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    return LaunchDescription(
+        [
+            Node(
+                package="hardware",
+                executable="hardware",
+                name="hardware",
+                output="screen",
+                parameters=[
+                    # {"simulation/model_file": xml_file},
+                ],
+                emulate_tty=True,
+                arguments=[("__log_level:=debug")],
+            ),
+        ]
+    )
