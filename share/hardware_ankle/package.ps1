2e72d81f045f4282e14a3352001175155e7c1548
liufq <popsay@163.com>
2025-11-10 18:17:35 +0800

    1.add elf3

--- share/hardware_ankle/package.ps1
+++ share/hardware_ankle/package.ps1
@@ -112,7 +112,6 @@ $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.Ful
 
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/hardware_ankle/hook/cmake_prefix_path.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/hardware_ankle/hook/ld_library_path_lib.ps1"
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/hardware_ankle/hook/pythonpath.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/hardware_ankle/local_setup.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
--- share/mujoco/package.ps1
+++ share/mujoco/package.ps1
@@ -110,10 +110,9 @@ function colcon_package_source_powershell_script {
 # the prefix is two levels up from the package specific share directory
 $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.FullName
 
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/mujoco/hook/cmake_module_path.ps1" /home/u01/code/bxirobotics/bxi_ros2_pkg_src/install/share/mujoco/hook/cmake_module_path.dsv /home/u01/code/bxirobotics/bxi_ros2_pkg_src/install/share/mujoco/hook/cmake_module_path.sh
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/mujoco/hook/cmake_module_path.ps1" /opt/bxi/bxi_ros2_pkg/share/mujoco/hook/cmake_module_path.dsv /opt/bxi/bxi_ros2_pkg/share/mujoco/hook/cmake_module_path.sh
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/mujoco/hook/cmake_prefix_path.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/mujoco/hook/ld_library_path_lib.ps1"
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/mujoco/hook/pythonpath.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/mujoco/local_setup.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
